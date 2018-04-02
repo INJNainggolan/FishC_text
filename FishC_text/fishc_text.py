fc893cb848908088172f216fa914054516838b87
Nainggolan <Nainggolan@github.com>
2018-04-02 09:09:42 +0800

    e

---
+++ FishC_text/fishc_text.py
@@ -0,0 +1,16 @@
+favourite = "FishC"
+for i in favourite:
+    print(i, end=" ")
+    print(i)
+
+mander = ["nima", "e", "salahei", "niubi", "hehe"]
+for each in mander:
+    print(each, len(each))
+
+for i in range(10):
+    if i%2 != 0:
+        print(i)
+        continue
+    i += 2
+    print(i)
+
